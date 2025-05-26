325bff21c91374f7932bff42c8f739f32914b9c4
lidia.vidal <lidia.vidal@estudante.ifto.edu.br>
2025-05-26 21:44:40 +0000

    tentei traduriz - deu errado

--- database/migrations/2025_04_30_125222_create_accountings_table.php
+++ database/migrations/2025_04_30_125222_create_accountings_table.php
@@ -33,8 +33,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('accountings');
-        Schema::table('accountings', function (Blueprint $table) {
-            $table->dropColumn(['date', 'competence_month']);
-        });
+    
     }
 };
